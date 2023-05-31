278e9c689bd95b9c32e0707a04665f6ad6a257f1
arefin6 <=arefinhossain3@gmial.com>
2023-05-31 11:05:24 +0600

    Problem8 solved

--- Problem8.ts
+++ Problem8.ts
@@ -1,1 +1,14 @@
 // Create a TypeScript program that declares a function that takes a string parameter with a literal type of "red", "green", or "blue", and an optional boolean parameter. If the boolean parameter is true, log the string parameter in uppercase. If the boolean parameter is false or not provided, log the string parameter in lowercase.
+
+function printColor(color:"red"|"green"|"blue",bol?:boolean|null){
+    if(bol){
+        console.log(color.toUpperCase())
+    }
+    else{
+        console.log(color.toLowerCase())
+    }
+
+}
+
+
+printColor("red")
